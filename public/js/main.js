15a36e62a96886b03b39d1dd81c61852ed6e8759
Marvin Murmann <m.murmann@nextint.de>
2021-04-15 01:00:04 +0200

    comments

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,23 @@
 $(function(){
 
+    // large viewer functionality
     $('.gallery img').click(function(){
+        // save the clicked object
         let clickedImage = $(this);
-        console.log(clickedImage.attr('src'));
-        
+        // set the image's source to that of the clicked element
         $('#img-large').attr('src', clickedImage.attr('src'));
+        // display the large viewer
         $('.gallery-viewer').css('display', 'flex');
     })
+
+    // close functionality, either click or esc key
     $('.exit').click(function(){
-        $('.gallery-viewer').css('display', 'none');
+        $('.gallery-viewer').hide();
+    })
+    $(document).keydown(function(e){
+        var c = e.key;
+        if(c == "Escape"){
+            $('.gallery-viewer').hide();
+        }
     })
 })
